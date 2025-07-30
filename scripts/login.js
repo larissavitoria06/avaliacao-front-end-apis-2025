cbcd66c8f7e6bfa3cf4337fb265d91f47afbc5b9
larissavitoria06 <larissavitoria.ssntos12@gmail.com>
2025-07-30 13:56:22 -0300

    commit

--- scripts/login.js
+++ scripts/login.js
@@ -1,27 +1,38 @@
-document.getElementById('loginForm').addEventListener('submit', async function (e) {
-    e.preventDefault();
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    try {
-        const response = await fetch('https://dummyjson.com/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            localStorage.setItem('authToken', data.token);  
-            window.location.href = './posts.html';  
-        } else {
-            document.getElementById('error-message').style.display = 'block';  
-        }
-    } catch (error) {
-        console.error('Erro de autenticação:', error);
-    }
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  login(); 
 });
+
+async function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  try {
+        const response = await fetch('https://dummyjson.com/auth/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    
+    username: username,
+    password: password,
+    expiresInMins: 30, 
+  }),
+})
+
+    const data = await response.json();
+
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      window.location.href = 'posts.html';
+    } else {
+      alert('Email ou senha incorretos');
+    }
+
+  } catch (error) {
+    console.error('Erro ao fazer login:', error);
+    alert('Erro ao fazer login');
+  }
+}
+
+// user:emilys
+//senha:emilyspass
